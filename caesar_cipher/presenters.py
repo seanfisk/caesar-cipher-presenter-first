03ac8822baceff6982abe2688ae74b001b953efb
Sean Fisk <sean@seanfisk.com>
2012-10-12 11:59:30 -0400

    Add register_for_events method.

--- caesar_cipher/presenters.py
+++ caesar_cipher/presenters.py
@@ -3,5 +3,13 @@ def init_presenters():
     ApplicationPresenter()
 
 class ApplicationPresenter(object):
-    def __init__(self):
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
+        self.register_for_events()
+
+    def user_submits(self):
         pass
+
+    def register_for_events(self):
+        self.view.when_user_submits(self.user_submits)
--- tests/test_presenters.py
+++ tests/test_presenters.py
@@ -2,7 +2,7 @@ import unittest
 
 from ludibrio import Mock
 
-from caesar_cipher.presenters import init_presenters
+from caesar_cipher.presenters import init_presenters, ApplicationPresenter
 
 class TestInitPresenters(unittest.TestCase):
     def test_init_presenters(self):
@@ -11,3 +11,21 @@ class TestInitPresenters(unittest.TestCase):
             ApplicationPresenter() >> None
         init_presenters()
         caesar_cipher.validate()
+
+class TestApplicationPresenter(unittest.TestCase):
+    def setUp(self):
+        self.mock_model = Mock()
+        self.mock_view = Mock()
+        self.presenter = ApplicationPresenter(self.mock_model, self.mock_view)
+
+    def test_register_for_events(self):
+        with self.mock_view:
+            self.mock_view.when_user_submits(
+                self.presenter.user_submits) >> None
+        with self.mock_model:
+            pass
+        self.presenter.register_for_events()
+
+    def tearDown(self):
+        self.mock_model.validate()
+        self.mock_view.validate()
