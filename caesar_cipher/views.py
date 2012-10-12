096d2990a5a1d816029afa773ca332c101697d30
Gray Gwizdz <gray.gwizdz@gmail.com>
2012-10-12 14:08:42 -0400

    Working GUI for caesar cipher

--- caesar_cipher/views.py
+++ caesar_cipher/views.py
@@ -4,7 +4,36 @@ class ApplicationView(QtGui.QWidget):
     
     def __init__(self, parent=None):
         super(ApplicationView,self).__init__(parent)
+        
+        self.layout = QtGui.QFormLayout(self)
+        
+        self.message_input = QtGui.QLineEdit(self)
+        self.layout.addRow('Message',self.message_input)
+        
+        self.key_input = QtGui.QLineEdit(self)
+        self.layout.addRow('Key',self.key_input)
+        
+        self.result = QtGui.QLabel(self)
+        self.layout.addRow('Result',self.result)
+        
+        self.submit = QtGui.QPushButton('Encode',self)
+        self.submit.clicked.connect(self.submit_clicked)
+        self.layout.addRow(self.submit)
+        
         self.show()
         self.raise_()
+    
     def when_user_submits(self,callback):
-        pass
+        self.submit_callback = callback
+
+    def get_message(self):
+        return self.message_input.text()
+
+    def get_key(self):
+        return int(self.key_input.text())
+
+    def set_result(self,result):
+        self.result.setText(result)
+
+    def submit_clicked(self):
+        self.submit_callback()
