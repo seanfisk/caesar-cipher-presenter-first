698acbc117978ced8a84f54059639679916afe87
Gray Gwizdz <gray.gwizdz@gmail.com>
2012-10-12 14:16:53 -0400

    Callback improvements

--- caesar_cipher/views.py
+++ caesar_cipher/views.py
@@ -5,6 +5,7 @@ class ApplicationView(QtGui.QWidget):
     def __init__(self, parent=None):
         super(ApplicationView,self).__init__(parent)
         
+        self.submit_callback = None
         self.layout = QtGui.QFormLayout(self)
         
         self.message_input = QtGui.QLineEdit(self)
@@ -17,14 +18,16 @@ class ApplicationView(QtGui.QWidget):
         self.layout.addRow('Result',self.result)
         
         self.submit = QtGui.QPushButton('Encode',self)
-        self.submit.clicked.connect(self.submit_clicked)
         self.layout.addRow(self.submit)
         
         self.show()
         self.raise_()
     
     def when_user_submits(self,callback):
+        if self.submit_callback:
+            self.submit.clicked.disconnect(self.submit_callback)
         self.submit_callback = callback
+        self.submit.clicked.connect(self.submit_callback)
 
     def get_message(self):
         return self.message_input.text()
@@ -34,6 +37,3 @@ class ApplicationView(QtGui.QWidget):
 
     def set_result(self,result):
         self.result.setText(result)
-
-    def submit_clicked(self):
-        self.submit_callback()
