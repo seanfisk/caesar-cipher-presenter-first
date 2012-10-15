508a43bd27f2662a7f1a8f1c8263e1f1ddef1ee4
Sean Fisk <sean@seanfisk.com>
2012-10-14 21:58:54 -0400

    Fix return value for get_key in tests.

--- tests/test_presenters.py
+++ tests/test_presenters.py
@@ -15,9 +15,9 @@ class TestApplicationPresenter(unittest.TestCase):
         self.mock_view.when_user_submits.assert_called_once_with(
             self.presenter._user_submits)
 
-    def test_user_submits(self):
+    def test_user_submits_correct_format(self):
         self.mock_view.get_message.return_value = 'abcd'
-        self.mock_view.get_key.return_value = 1
+        self.mock_view.get_key.return_value = '1'
         self.mock_model.caesar_encode.return_value = 'bcde'
         self.mock_view.set_result.return_value = None
 
