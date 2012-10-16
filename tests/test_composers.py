ddb904d7eb73e1e275d474fa299eff066679396e
Sean Fisk <sean@seanfisk.com>
2012-10-16 00:14:32 -0400

    Patch the entire PySide module.

--- tests/test_composers.py
+++ tests/test_composers.py
@@ -3,10 +3,13 @@ import unittest
 from mock import patch, sentinel, call, MagicMock
 
 
-# We don't want to require PySide.QtGui for testing, since we are not
-# testing our user interface. However, we may require PySide.QtCore
-# since we are using Qt's signals and slots mechanism.
-@patch('PySide.QtGui', MagicMock())
+# We don't want to require `PySide.QtGui' for testing, since we are
+# not testing our user interface. However, we may require
+# `PySide.QtCore' since we are using Qt's signals and slots
+# mechanism. Since the `composers' module under test doesn't require
+# either `PySide.QtGui' or PySide.QtCore, just patch the entire
+# `PySide' module.
+@patch.dict('sys.modules', {'PySide': MagicMock()})
 class TestCreateQtPresenter(unittest.TestCase):
 
     @patch('caesar_cipher.composers.ApplicationPresenter', autospec=True)
